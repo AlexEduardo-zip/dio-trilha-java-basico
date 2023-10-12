c936f92f3877d501df3327b9099016edad5a6b88
Alex Eduardo <alexeduardoaeas@gmail.com>
2023-10-12 17:07:58 -0300

    Criando arquivos base

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+/*
+* Classe ContaTerminal
+* Classe que representa a interface de terminal para o usuário
+* @autor: AlexEduardo-zip
+* @version: 1.0
+* @since: 2021-03-10
+*/
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Por favor, digite o nome da Agência !");
+
+        System.out.println("Olá,  obrigado p");
+    }
+}
